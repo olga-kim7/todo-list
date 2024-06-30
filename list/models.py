e61a25cbfd44f08bfe74cea53be66691b760a00c
Olga <olgahrytsiuk777@gmail.com>
2024-07-01 00:45:25 +0200

    add models.py

--- list/models.py
+++ list/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    optional_dead_date = models.DateTimeField(null=True, blank=True)
+    boolean_field = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.content
